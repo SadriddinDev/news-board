f2dba1626b220c1ea7f5b2696dae78c035488bfa
Sadriddin <sadriddindev@gmail.com>
2022-01-22 11:31:08 +0500

    Checked flake8 and push herkou

--- NewsAPI/settings.py
+++ NewsAPI/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,5 @@ CELERY_RESULT_BACKEND = "redis://redis:6379"
 CELERY_TIMEZONE = "UTC"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
+
+django_heroku.settings(locals())
